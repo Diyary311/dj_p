7544706d0871d6078f89edc3fc2137f7842ca4f1
Diyary311 <arazomar500@gmail.com>
2025-05-10 00:27:22 +0300

    Add files via upload

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def projects(request):
+    return render(request, 'main/projects.html')
+
+def blog(request):
+    return render(request, 'main/blog.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
